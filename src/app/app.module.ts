69a0e55cbb36b1a5dd9a6c04e9eae8a445e09d27
Esaúl <f.esaul.franco.r@gmail.com>
2020-11-25 20:22:31 -0600

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,18 @@ import { AppComponent } from './app.component';
 import { cardlistcomponent } from './Components/cardlist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material/material.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { HomescreenComponent } from './homescreen/homescreen.component';
 import { TablaMedicosComponent } from './tabla-medicos/tabla-medicos.component';
-
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule} from '@angular/material/paginator';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -42,7 +42,7 @@ import {MatInputModule} from '@angular/material/input';
     MatSortModule,
     MatPaginatorModule,
     MatInputModule,
-  
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
